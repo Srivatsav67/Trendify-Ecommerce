ed8758e46650dd4ecd73c26c2137523389839841
Srivatsav Venkatayogi <134275585+Srivatsav67@users.noreply.github.com>
2025-04-12 11:54:16 +0530

    Update orders.js

--- src/dummy/orders.js
+++ src/dummy/orders.js
@@ -1,1 +1,6 @@
-export const orders = [{ id: 1, userId: 1, total: 2998, status: 'Shipped' }];
+const orders = [
+  { id: "ORD123", userId: 1, items: [1, 2], total: 2298, status: "Processing" },
+  { id: "ORD124", userId: 2, items: [2], total: 699, status: "Shipped" },
+];
+
+export default orders;
